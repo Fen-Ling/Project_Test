821922b2f9f8156dd67af9ff2f7cf919e67a82c0
Fen-Ling <arha5168@gmail.com>
2024-11-05 11:30:35 +0300

    Stone_prefab

--- Assets/Scripts/Stick.cs
+++ Assets/Scripts/Stick.cs
@@ -27,11 +27,11 @@ namespace Golf
         {
             m_isDown = true;
         }
-        private void Update()
-        {
-            m_dir = (point.position - m_lastPointPosition).normalized;
-            m_lastPointPosition = point.position;            
-        }
+        // private void Update()
+        // {
+        //     m_dir = (point.position - m_lastPointPosition).normalized;
+        //     m_lastPointPosition = point.position;            
+        // }
         private void FixedUpdate()
         {
             Vector3 angle = transform.localEulerAngles;
@@ -52,8 +52,8 @@ namespace Golf
                 stone.isDirty = true;
 
                 var contact = other.contacts[0];
-                // other.rigidbody.AddForce(-contact.normal * power, ForceMode.Impulse);
-                other.rigidbody.AddForce(m_dir * power, ForceMode.Impulse);
+                other.rigidbody.AddForce(-contact.normal * power, ForceMode.Impulse);
+                // other.rigidbody.AddForce(m_dir * power, ForceMode.Impulse);
                 onCollisionStone?.Invoke();
             }
         }
